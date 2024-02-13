87ddc79278a4ab2f442935e9902ba3d24470d48a
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-02-12 17:26:07 -0700

    Update main.js

--- dontopen/Ruffletest/main.js
+++ dontopen/Ruffletest/main.js
@@ -3,8 +3,8 @@ var swfobject = {};
 swfobject.embedSWF = function(url, cont, width, height){
     var ruffle = window.RufflePlayer.newest(),
         player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            width: width,
-            height: height,
+            width: 1800,
+            height: 900,
             style: 'width: ' + width + 'px; height: ' + height + 'px',
         });
 
